3bdcd70440012cebc84460f6fd5770bff7399fd4
Student <student@d17-431-pc-09.ipa.ii.agh.edu.pl>
2025-02-08 14:42:23 +0100

    ivoice fix

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,58 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private HashMap<Product, Integer> products = new HashMap<Product, Integer>();
+    private BigDecimal subTotal = BigDecimal.ZERO;
+    private BigDecimal tax = BigDecimal.ZERO;
+    private BigDecimal total = BigDecimal.ZERO;
 
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("Product cannot be null");
+        } else {
+            products.put(product, 1);
+        }
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity<=0) {
+            throw new IllegalArgumentException("Quantity cannot be less than 0");
+        } else {
+            products.put(product, quantity);
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        if (this.products != null) {
+            for (Product product : products.keySet()) {
+                subTotal = subTotal.add(product.getPrice().multiply(BigDecimal.valueOf(products.get(product))));
+            }
+        }
+        return subTotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+        if (this.products != null) {
+            for (Product product : products.keySet()) {
+                tax = tax.add(product.getPrice().multiply(product.getTaxPercent().multiply(BigDecimal.valueOf(products.get(product)))));
+            }
+        }
+        return tax;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        if (this.products != null) {
+            for (Product product : products.keySet()) {
+                total = total.add(product.getPriceWithTax().multiply(BigDecimal.valueOf(products.get(product))));
+            }
+        }
+        return total;
     }
 }
--- src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
+++ src/main/java/pl/edu/agh/mwo/invoice/product/Product.java
@@ -10,24 +10,31 @@ public abstract class Product {
     private final BigDecimal taxPercent;
 
     protected Product(String name, BigDecimal price, BigDecimal tax) {
-        this.name = name;
-        this.price = price;
-        this.taxPercent = tax;
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.name = name;
+            this.price = price;
+            this.taxPercent = tax;
+        }
     }
 
     public String getName() {
-        return null;
+
+        return this.name;
     }
 
     public BigDecimal getPrice() {
-        return null;
+        return this.price;
     }
 
     public BigDecimal getTaxPercent() {
-        return null;
+        return this.taxPercent;
     }
 
     public BigDecimal getPriceWithTax() {
-        return null;
+        return this.price.add(this.price.multiply(this.taxPercent));
     }
 }
