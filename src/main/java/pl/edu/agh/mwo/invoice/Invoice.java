4874ffd11fbf2e25226b79ed5e10b831c2c0f0bf
Julia Sabała <julia.sabala@princity.com>
2025-06-15 17:12:45 +0200

    invoicing methods

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -3,7 +3,8 @@ package pl.edu.agh.mwo.invoice;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.util.Iterator;
+import java.util.Map;
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
@@ -14,10 +15,7 @@ public class Invoice {
     }
 
     public void addProduct(Product product, Integer quantity) {
-        if (product == null || quantity <= 0) {
-            throw new IllegalArgumentException();
-        }
-        products.put(product, quantity);
+        this.products.put(product, 1);
     }
 
     public BigDecimal getNetTotal() {
@@ -27,6 +25,35 @@ public class Invoice {
             totalNet = totalNet.add(product.getPrice().multiply(quantity));
         }
         return totalNet;
+    }
+
+    public BigDecimal getSubtotal() {
+        return getTotal().subtract(getTax());
+    }
+
+    public BigDecimal getTax() {
+        BigDecimal totalTax = new BigDecimal(0);
+        Iterator<Map.Entry<Product, Integer>> it = products.entrySet().iterator();
+
+        while (it.hasNext()) {
+            Map.Entry<Product, Integer> pair = it.next();
+            BigDecimal productTax = pair.getKey().getTaxOnly();
+            BigDecimal quantity = new BigDecimal(pair.getValue());
+            totalTax = totalTax.add(productTax.multiply(quantity));
+        }
+        return totalTax;
+    }
+
+    public BigDecimal getTotal() {
+        BigDecimal total = new BigDecimal(0);
+        Iterator<Map.Entry<Product, Integer>> it = products.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<Product, Integer> pair = it.next();
+            BigDecimal productPriceWithTax = pair.getKey().getPriceWithTax();
+            BigDecimal quantity = new BigDecimal(pair.getValue());
+            total = total.add(productPriceWithTax.multiply(quantity));
+        }
+        return total;
     }
 
     public BigDecimal getTaxTotal() {
@@ -41,4 +68,6 @@ public class Invoice {
         }
         return totalGross;
     }
+
+}
 }
